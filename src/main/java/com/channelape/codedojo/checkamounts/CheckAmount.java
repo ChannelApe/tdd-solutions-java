a482dd77295a06fd2bd36df3a1d252e9d961496b
Ryan Kazokas <rkazokas@channelape.com>
2018-09-14 12:32:16 -0400

    Add billions to check amounts

--- src/main/java/com/channelape/codedojo/checkamounts/CheckAmount.java
+++ src/main/java/com/channelape/codedojo/checkamounts/CheckAmount.java
@@ -6,36 +6,36 @@ import java.util.Map;
 public class CheckAmount {
 
 	private final String value;
-	private static final Map<Integer, String> digitsToWords;
+	private static final Map<Long, String> digitsToWords;
 	static {
 		digitsToWords = new HashMap<>();
-		digitsToWords.put(1, "one");
-		digitsToWords.put(2, "two");
-		digitsToWords.put(3, "three");
-		digitsToWords.put(4, "four");
-		digitsToWords.put(5, "five");
-		digitsToWords.put(6, "six");
-		digitsToWords.put(7, "seven");
-		digitsToWords.put(8, "eight");
-		digitsToWords.put(9, "nine");
-		digitsToWords.put(10, "ten");
-		digitsToWords.put(11, "eleven");
-		digitsToWords.put(12, "twelve");
-		digitsToWords.put(13, "thirteen");
-		digitsToWords.put(14, "fourteen");
-		digitsToWords.put(15, "fifteen");
-		digitsToWords.put(16, "sixteen");
-		digitsToWords.put(17, "seventeen");
-		digitsToWords.put(18, "eighteen");
-		digitsToWords.put(19, "nineteen");
-		digitsToWords.put(20, "twenty");
-		digitsToWords.put(30, "thirty");
-		digitsToWords.put(40, "forty");
-		digitsToWords.put(50, "fifty");
-		digitsToWords.put(60, "sixty");
-		digitsToWords.put(70, "seventy");
-		digitsToWords.put(80, "eighty");
-		digitsToWords.put(90, "ninety");
+		digitsToWords.put(1L, "one");
+		digitsToWords.put(2L, "two");
+		digitsToWords.put(3L, "three");
+		digitsToWords.put(4L, "four");
+		digitsToWords.put(5L, "five");
+		digitsToWords.put(6L, "six");
+		digitsToWords.put(7L, "seven");
+		digitsToWords.put(8L, "eight");
+		digitsToWords.put(9L, "nine");
+		digitsToWords.put(10L, "ten");
+		digitsToWords.put(11L, "eleven");
+		digitsToWords.put(12L, "twelve");
+		digitsToWords.put(13L, "thirteen");
+		digitsToWords.put(14L, "fourteen");
+		digitsToWords.put(15L, "fifteen");
+		digitsToWords.put(16L, "sixteen");
+		digitsToWords.put(17L, "seventeen");
+		digitsToWords.put(18L, "eighteen");
+		digitsToWords.put(19L, "nineteen");
+		digitsToWords.put(20L, "twenty");
+		digitsToWords.put(30L, "thirty");
+		digitsToWords.put(40L, "forty");
+		digitsToWords.put(50L, "fifty");
+		digitsToWords.put(60L, "sixty");
+		digitsToWords.put(70L, "seventy");
+		digitsToWords.put(80L, "eighty");
+		digitsToWords.put(90L, "ninety");
 	}
 
 	public CheckAmount(final String value) {
@@ -51,43 +51,59 @@ public class CheckAmount {
 	}
 
 	private StringBuilder determineSpelledOutDollarAmount(final String amount) {
-		final int integerAmount = Integer.parseInt(amount);
+		final long integerAmount = Long.parseLong(amount);
 		if (integerAmount < 1000) {
 			return determineHundredsGroup(integerAmount);
 		}
 		if (integerAmount < 1000000) {
-			final int thousandsAmount = integerAmount / 1000;
+			final long thousandsAmount = integerAmount / 1000L;
 			final StringBuilder hundredsAmountString = determineHundredsGroup(integerAmount - (thousandsAmount * 1000));
 			final StringBuilder thousandsAmountString = determineHundredsGroup(thousandsAmount);
 			return new StringBuilder().append(thousandsAmountString).append(" thousand ").append(hundredsAmountString);
+		} else if (integerAmount < 1000000000) {
+
+			final long millionsAmount = integerAmount / 1000000;
+			final long thousandsAmount = (integerAmount - (millionsAmount * 1000000)) / 1000;
+			final long hundredsAmount = (integerAmount - (millionsAmount * 1000000) - (thousandsAmount * 1000));
+			final StringBuilder hundredsAmountString = determineHundredsGroup(hundredsAmount);
+			final StringBuilder thousandsAmountString = determineHundredsGroup(thousandsAmount);
+			final StringBuilder millionsAmountString = determineHundredsGroup(millionsAmount);
+			return new StringBuilder().append(millionsAmountString).append("million ").append(thousandsAmountString)
+					.append(" thousand ").append(hundredsAmountString);
 		}
-		final int millionsAmount = integerAmount / 1000000;
-		final int thousandsAmount = (integerAmount - (millionsAmount * 1000000)) / 1000;
-		final int hundredsAmount = (integerAmount - (millionsAmount * 1000000) - (thousandsAmount * 1000));
+
+		final long billionsAmount = integerAmount / 1000000000;
+		final long millionsAmount = (integerAmount - (billionsAmount * 1000000000)) / 1000000;
+		final long thousandsAmount = (integerAmount - (billionsAmount * 1000000000) - (millionsAmount * 1000000))
+				/ 1000;
+		final long hundredsAmount = (integerAmount - (millionsAmount * 1000000) - (billionsAmount * 1000000000)
+				- (thousandsAmount * 1000));
 		final StringBuilder hundredsAmountString = determineHundredsGroup(hundredsAmount);
 		final StringBuilder thousandsAmountString = determineHundredsGroup(thousandsAmount);
 		final StringBuilder millionsAmountString = determineHundredsGroup(millionsAmount);
-		return new StringBuilder().append(millionsAmountString).append("million ").append(thousandsAmountString)
-				.append(" thousand ").append(hundredsAmountString);
+		final StringBuilder billionsAmountString = determineHundredsGroup(billionsAmount);
+
+		return new StringBuilder().append(billionsAmountString).append(" billion ").append(millionsAmountString)
+				.append("million ").append(thousandsAmountString).append(" thousand ").append(hundredsAmountString);
 	}
 
-	private StringBuilder determineHundredsGroup(int amount) {
+	private StringBuilder determineHundredsGroup(long amount) {
 		final StringBuilder spelledOutDollarAmountStringBuilder = new StringBuilder();
 		if (digitsToWords.containsKey(amount)) {
 			spelledOutDollarAmountStringBuilder.append(digitsToWords.get(amount));
 		} else {
-			final int hundreds = amount / 100;
+			final long hundreds = amount / 100;
 			if (hundreds > 0) {
 				amount -= hundreds * 100;
 				spelledOutDollarAmountStringBuilder.append(digitsToWords.get(hundreds)).append(" hundred ");
 			}
 
-			final int tens = (amount / 10) * 10;
+			final long tens = (amount / 10) * 10;
 			if (tens > 0) {
 				spelledOutDollarAmountStringBuilder.append(digitsToWords.get(tens)).append('-');
 			}
 
-			final int ones = amount % 10;
+			final long ones = amount % 10;
 			if (ones > 0) {
 				spelledOutDollarAmountStringBuilder.append(digitsToWords.get(ones));
 			}
--- src/test/java/com/channelape/codedojo/checkamounts/CheckAmountTest.java
+++ src/test/java/com/channelape/codedojo/checkamounts/CheckAmountTest.java
@@ -114,7 +114,6 @@ public class CheckAmountTest {
 				actualSpelledOutCheckAmount);
 	}
 
-	// TODO: Ryans turn!
 	@Test
 	public void given40200673357DollarsAnd00CentsWhenSpellingOutCheckAmountThenExpectFortyBillionTwoHundredMillionSixHundSeventyThreeThousandThreeHundredFiftySevenAnd00Over100Dollars() {
 		final CheckAmount checkAmount = new CheckAmount("40200673357.00");
