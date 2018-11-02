abbc4e2b05aa5f1b7e5c8f78625189e73fa78279
R.J. Davis <rjdavis@channelape.com>
2018-11-02 11:02:28 -0400

    Added next code dojo problem.

---
+++ src/main/java/com/channelape/codedojo/balancedpasswords/BalancedPassword.java
@@ -0,0 +1,10 @@
+package com.channelape.codedojo.balancedpasswords;
+
+/**
+ *
+ * TDD solution for https://app.codility.com/programmers/task/balanced_password/
+ *
+ */
+public class BalancedPassword {
+
+}
---
+++ src/test/java/com/channelape/codedojo/balancedpasswords/BalancedPasswordTest.java
@@ -0,0 +1,12 @@
+package com.channelape.codedojo.balancedpasswords;
+
+import org.junit.Test;
+
+public class BalancedPasswordTest {
+
+	@Test
+	public void givenWhneThen() throws Exception {
+		new BalancedPassword();
+	}
+
+}
