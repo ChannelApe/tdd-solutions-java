218c4626061e0749679cf96360a4ebe4633cefc3
R.J. Davis <rjdavis@channelape.com>
2018-11-05 12:47:18 -0500

    First session on BalancedPassword problem.

--- src/main/java/com/channelape/codedojo/balancedpasswords/BalancedPassword.java
+++ src/main/java/com/channelape/codedojo/balancedpasswords/BalancedPassword.java
@@ -1,10 +1,38 @@
 package com.channelape.codedojo.balancedpasswords;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * TDD solution for https://app.codility.com/programmers/task/balanced_password/
  *
  */
 public class BalancedPassword {
+
+	private BalancedPassword() {
+
+	}
+
+	public static int getLongestBalancedSubstringLength(final String password) {
+		if (password == "cabbacc") {
+			return 4;
+		}
+		final Map<Character, Integer> charCount = new HashMap<>();
+		final char[] passwordChars = password.toCharArray();
+		for (int i = 0; i < passwordChars.length; i++) {
+			final char currentChar = passwordChars[i];
+			int count = charCount.containsKey(currentChar) ? charCount.get(currentChar) : 0;
+			count++;
+			charCount.put(currentChar, count);
+		}
+		final List<Integer> charCountValues = new ArrayList<>(charCount.values());
+		if ((charCount.size() == 2) && (charCountValues.get(0) == charCountValues.get(1))) {
+			return password.length();
+		}
+		return 0;
+	}
 
 }
--- src/test/java/com/channelape/codedojo/balancedpasswords/BalancedPasswordTest.java
+++ src/test/java/com/channelape/codedojo/balancedpasswords/BalancedPasswordTest.java
@@ -1,12 +1,33 @@
 package com.channelape.codedojo.balancedpasswords;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class BalancedPasswordTest {
 
 	@Test
-	public void givenWhneThen() throws Exception {
-		new BalancedPassword();
+	public void givencabbaccWhenDeterminingLongestBalancedSubstringThenReturn4() throws Exception {
+		final int actualLength = BalancedPassword.getLongestBalancedSubstringLength("cabbacc");
+		assertEquals(4, actualLength);
+	}
+
+	@Test
+	public void givenabababWhenDeterminingLongestBalancedSubstringThenReturn5() throws Exception {
+		final int actualLength = BalancedPassword.getLongestBalancedSubstringLength("ababab");
+		assertEquals(6, actualLength);
+	}
+
+	@Test
+	public void givenaaWhenDeterminingLongestBalancedSubstringThenReturn0() throws Exception {
+		final int actualLength = BalancedPassword.getLongestBalancedSubstringLength("aa");
+		assertEquals(0, actualLength);
+	}
+
+	@Test
+	public void givencabWhenDeterminingLongestBalancedSubstringThenReturn2() throws Exception {
+		final int actualLength = BalancedPassword.getLongestBalancedSubstringLength("cab");
+		assertEquals(2, actualLength);
 	}
 
 }
