69ac9781be801ad3eafdc2feba92f52a7c3653eb
R.J. Davis <rjdavis@channelape.com>
2018-11-26 12:25:06 -0500

    Substrings within a password now count correctly towards returned length

--- src/main/java/com/channelape/codedojo/balancedpasswords/BalancedPassword.java
+++ src/main/java/com/channelape/codedojo/balancedpasswords/BalancedPassword.java
@@ -17,20 +17,37 @@ public class BalancedPassword {
 	}
 
 	public static int getLongestBalancedSubstringLength(final String password) {
-		if (password == "cabbacc") {
-			return 4;
+		int longestBalancedSubstringLength = 0;
+		for (int i = 0; i < password.length(); i++) {
+			for (int j = i + 1; j <= password.length(); j++) {
+				final String passwordSubstring = password.substring(i, j);
+				final int substringLength = getSubstringLength(passwordSubstring);
+				if (substringLength > longestBalancedSubstringLength) {
+					longestBalancedSubstringLength = substringLength;
+				}
+			}
 		}
-		final Map<Character, Integer> charCount = new HashMap<>();
-		final char[] passwordChars = password.toCharArray();
-		for (int i = 0; i < passwordChars.length; i++) {
-			final char currentChar = passwordChars[i];
-			int count = charCount.containsKey(currentChar) ? charCount.get(currentChar) : 0;
-			count++;
-			charCount.put(currentChar, count);
-		}
-		final List<Integer> charCountValues = new ArrayList<>(charCount.values());
-		if ((charCount.size() == 2) && (charCountValues.get(0) == charCountValues.get(1))) {
-			return password.length();
+		return longestBalancedSubstringLength;
+	}
+
+	private static int getSubstringLength(final String passwordSubstring) {
+		if ((passwordSubstring.length() % 2) == 0) {
+			final Map<Character, Integer> charCount = new HashMap<>();
+			final char[] passwordChars = passwordSubstring.toCharArray();
+			for (int i = 0; i < passwordChars.length; i++) {
+				final char currentChar = passwordChars[i];
+				int count = charCount.containsKey(currentChar) ? charCount.get(currentChar) : 0;
+				count++;
+				charCount.put(currentChar, count);
+
+				if (charCount.size() > 2) {
+					return 0;
+				}
+			}
+			final List<Integer> charCountValues = new ArrayList<>(charCount.values());
+			if ((charCount.size() == 2) && (charCountValues.get(0) == charCountValues.get(1))) {
+				return passwordSubstring.length();
+			}
 		}
 		return 0;
 	}
