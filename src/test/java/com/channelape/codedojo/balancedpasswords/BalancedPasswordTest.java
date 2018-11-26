d61890419249afce43cc59561147f09f77971089
R.J. Davis <rjdavis@channelape.com>
2018-11-26 13:00:06 -0500

    Added test case that is failing based on execution time.

--- src/test/java/com/channelape/codedojo/balancedpasswords/BalancedPasswordTest.java
+++ src/test/java/com/channelape/codedojo/balancedpasswords/BalancedPasswordTest.java
@@ -1,8 +1,13 @@
 package com.channelape.codedojo.balancedpasswords;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
+
+import com.google.common.base.Stopwatch;
 
 public class BalancedPasswordTest {
 
@@ -28,6 +33,25 @@ public class BalancedPasswordTest {
 	public void givencabWhenDeterminingLongestBalancedSubstringThenReturn2() throws Exception {
 		final int actualLength = BalancedPassword.getLongestBalancedSubstringLength("cab");
 		assertEquals(2, actualLength);
+	}
+
+	// TODO: Ryan starts next week.
+	@Test
+	public void given99998csAndabWhenDeterminingLongestBalancedSubstringThenReturn2() throws Exception {
+		final StringBuilder passwordStringBuilder = new StringBuilder();
+		for (int i = 0; i < 99998; i++) {
+			passwordStringBuilder.append('c');
+		}
+		passwordStringBuilder.append("ab");
+
+		final String password = passwordStringBuilder.toString();
+		System.out.println("Password Length " + password);
+		final Stopwatch testStopwatch = Stopwatch.createStarted();
+		final int actualLength = BalancedPassword.getLongestBalancedSubstringLength(password);
+		testStopwatch.stop();
+		assertEquals(2, actualLength);
+		assertTrue(String.format("Expected run time of less than 2 minutes. Actual runtime was %s", testStopwatch),
+				2 <= testStopwatch.elapsed(TimeUnit.MINUTES));
 	}
 
 }
